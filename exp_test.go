276c75c4b38f4f41bcb3cbc8289de2a955d39797
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-08-28 15:42:55 +0100

    log 2x-3

--- exp_test.go
+++ exp_test.go
@@ -58,30 +58,39 @@ func TestExp64x8(t *testing.T) {
 func TestDeriveLogCoefs(t *testing.T) {
 	explicitTest(t, "LOG_COEFS")
 	rng := rand.New(rand.NewSource(42))
-	w := []float64{0, 1, -0.5, 1. / 3, -1. / 4, 1. / 5, -1. / 6, 1. / 7, -1. / 8, 1. / 9}
+	w := []float64{
+		math.Log(1.5),
+		1. / math.Pow(3, 1),
+		-1. / (2 * math.Pow(3, 2)),
+		1. / (3 * math.Pow(3, 3)),
+		-1. / (4 * math.Pow(3, 4)),
+		1. / (5 * math.Pow(3, 5)),
+		-1. / (6 * math.Pow(3, 6)),
+		1. / (7 * math.Pow(3, 7)),
+		-1. / (8 * math.Pow(3, 8)),
+	}
 	log1p := func(x float64) float64 {
-		y := w[0]
-		prod := x - 1
-		for i := 1; i < len(w); i++ {
+		y := 0.0
+		prod := 1.0
+		for i := 0; i < len(w); i++ {
 			y += prod * w[i]
-			prod *= x - 1
+			prod *= 2*x - 3
 		}
 		return y
 	}
 	loss := func() float64 {
 		err := 0.0
-		for x := 1.0; x < 2; x += 0.01 {
-			rel := (math.Log(x) - log1p(x))
+		n := 0.0
+		for x := 1.0; x <= 2.0; x += 0.05 {
+			rel := math.Abs(math.Log(x) - log1p(x))
 			err += rel * rel
+			n++
 		}
-		return err / (1.0 / 0.01)
+		return err / n
 	}
 	fmt.Printf("Before: %.18f\n", loss())
-	SPSA64(w, loss, 1e-3, 1e-1, 10e5, rng)
+	SPSA64(w, loss, 1e-4, 2e-1, 2e6, rng)
 	fmt.Printf("After:  %.18f\n", loss())
-	for i := range w {
-		fmt.Printf("%.12f, 1/%.12f\n", w[i], 1/w[i])
-	}
 	fmt.Printf(`
 	_c0 := archsimd.BroadcastFloat64x8(%.12f)
 	_c1 := archsimd.BroadcastFloat64x8(1.0 / %.12f)
@@ -92,28 +101,33 @@ func TestDeriveLogCoefs(t *testing.T) {
 	_c6 := archsimd.BroadcastFloat64x8(1.0 / %.12f)
 	_c7 := archsimd.BroadcastFloat64x8(1.0 / %.12f)
 	_c8 := archsimd.BroadcastFloat64x8(1.0 / %.12f)
-	_c9 := archsimd.BroadcastFloat64x8(1.0 / %.12f)
 `,
-		w[0], 1./w[1], 1./w[2], 1./w[3], 1./w[4], 1./w[5], 1./w[6], 1./w[7], 1./w[8], 1./w[9])
+		w[0], 1./w[1], 1./w[2], 1./w[3], 1./w[4], 1./w[5], 1./w[6], 1./w[7], 1./w[8])
 	println()
 }
 
 func TestLog64x8(t *testing.T) {
-	x := []float64{0.01, 0.1, 0.25, 0.5, 0.9, 1, 1.5, 2, 2.2, 2.718, 3, 4, 5, 20, 1000}
+	x := []float64{0.01, 0.1, 0.25, 0.5, 0.9, 1.5, 2, 2.2, 2.718, 3, 4, 5, 20, 1000}
 	y := make([]float64, len(x))
 	for i := range y {
 		y[i] = math.Log(x[i])
 	}
 	Log64x8(x, x)
-	assertRelEqual(t, y, x, 0.0001)
+	assertRelEqual(t, y, x, 0.0000001)
 	// printError("Log64x8", y, x)
+	x[0] = 1.0
+	Log64x8(x[:1], y[:1])
+	if math.Abs(y[0]) > 0.00000001 {
+		t.Fatal("1.0 edge case")
+	}
 }
 
 func TestInverseExpLog(t *testing.T) {
+	rng := rand.New(rand.NewSource(42))
 	x := make([]float64, 1024)
 	y := make([]float64, len(x))
 	for i := range x {
-		x[i] = rand.NormFloat64() * 10
+		x[i] = rng.NormFloat64() * 10
 	}
 	Exp64x8(x, y)
 	Log64x8(y, y)
@@ -123,16 +137,17 @@ func TestInverseExpLog(t *testing.T) {
 			err = math.Abs(x[i] - y[i])
 		}
 	}
-	if err > 0.005 {
+	if err > 0.0002 {
 		t.Fatalf("TestInverses: abs err %.12f too high!", err)
 	}
 }
 
 func TestInverseLogExp(t *testing.T) {
+	rng := rand.New(rand.NewSource(42))
 	x := make([]float64, 1024)
 	y := make([]float64, len(x))
 	for i := range x {
-		x[i] = rand.NormFloat64() * 25
+		x[i] = rng.NormFloat64() * 25
 		if x[i] < 0 {
 			x[i] = -x[i]
 		}
@@ -145,7 +160,7 @@ func TestInverseLogExp(t *testing.T) {
 			err = math.Abs(x[i] - y[i])
 		}
 	}
-	if err > 0.005 {
+	if err > 0.00002 {
 		t.Fatalf("TestInverses: abs err %.12f too high!", err)
 	}
 }
